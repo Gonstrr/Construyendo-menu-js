bab4b97a81530ec1a9830fc237fe7b34e6e22164
Gonstrr <gonzalo.salas.tr@gmail.com>
2021-10-08 18:40:49 -0300

    listo

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ const { inquirerMenu,
 } = require('./helpers/inquirer');
 
 const Tareas = require('./models/tareas');
-
 const main = async () => {
     let opt ='';
     const tareas = new Tareas();
